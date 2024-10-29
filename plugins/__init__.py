f802bb1d0fe5ef139e08e4d133cbc4945618a5a9
SAHU <161798305+viratvijay143@users.noreply.github.com>
2024-10-29 19:05:27 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -9,14 +9,14 @@ TO_CHATS = set(int(x) for x in os.environ.get("TO_CHATS", "").split())
 # required to Movies information
 OMDB_KEY = environ.get("OMDB_KEY", "")
 
-RE1TXT = os.environ.get("RE1TXT", "@Username1")
-RE2TXT = os.environ.get("RE2TXT", "@Username1")
-RE3TXT = os.environ.get("RE3TXT", "⚠️ Uploaded By @HQFilms4U")
-RE4TXT = os.environ.get("RE4TXT", "@Username3")
-RE5TXT = os.environ.get("RE5TXT", "@Username4")
-RE6TXT = os.environ.get("RE6TXT", "@Username5")
+RE1TXT = os.environ.get("RE1TXT", "@vijaysahu_2")
+RE2TXT = os.environ.get("RE2TXT", "@vijaysahu_2")
+RE3TXT = os.environ.get("RE3TXT", "⚠️ Uploaded By @vijaysahu_2")
+RE4TXT = os.environ.get("RE4TXT", "@vijaysahu_2")
+RE5TXT = os.environ.get("RE5TXT", "@vijaysahu_2")
+RE6TXT = os.environ.get("RE6TXT", "@vijaysahu_2")
 
 # text you want to replace with text above.
-REPLACED = os.environ.get("REPLACED", "@Hollywood_0980")
+REPLACED = os.environ.get("REPLACED", "@AestheticsOfStubborn")
 
 # MEDIA_FORWARD_ID = int(os.environ.get("MEDIA_FORWARD_ID"))
